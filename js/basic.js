36152bb7a8b4fb2c0f69649fcd166c46bc726106
enkikkeli <onni.riikonen@student.lut.fi>
2024-09-12 16:32:48 +0300

    5

--- js/basic.js
+++ js/basic.js
@@ -8,54 +8,58 @@ function addData(event) {
     const file = image.files[0];
 
 
-    const reader = new FileReader();
-    reader.onload = function(f) {
-        const imageUrl = f.target.result;
+    const table = document.getElementById("table");
+    const rows = document.getElementsByTagName("tr");
+    let isUsername = false;
 
-        const table = document.getElementById("table");
-        const rows = document.getElementsByTagName("tr");
-        let isUsername = false;
-
-        for (let i = 0; i < rows.length; i++) {
-            const data = rows[i].getElementsByTagName("td");
-            if (data.length > 0 && data[0].textContent == username) {
-                data[1].textContent = email;
-                data[2].textContent = admin;
-                isUsername = true;
-                break;
-            }
+    for (let i = 0; i < rows.length; i++) {
+        const data = rows[i].getElementsByTagName("td");
+        if (data.length > 0 && data[0].textContent == username) {
+            data[1].textContent = email;
+            data[2].textContent = admin;
+            isUsername = true;
+            break;
         }
-
-
-        if (isUsername == false) {
-            const newRow = document.createElement("tr");
-
-            const usernameInput = document.createElement("td");
-            usernameInput.textContent = username;
-            newRow.appendChild(usernameInput);
-
-            const emailInput = document.createElement("td");
-            emailInput.textContent = email;
-            newRow.appendChild(emailInput);
-
-            const adminInput = document.createElement("td");
-            adminInput.textContent = admin;
-            newRow.appendChild(adminInput);
-
-            const imageInput = document.createElement('td');
-            const newImage = document.createElement("img");
-            newImage.src = imageUrl;
-            newImage.width = 64;
-            newImage.height = 64;
-            imageInput.appendChild(newImage);
-            newRow.appendChild(imageInput);
-
-            table.appendChild(newRow);
-        }
-
     }
 
-    reader.readAsDataURL(file);
+
+    if (isUsername == false) {
+        const newRow = document.createElement("tr");
+
+        const usernameInput = document.createElement("td");
+        usernameInput.textContent = username;
+        newRow.appendChild(usernameInput);
+
+        const emailInput = document.createElement("td");
+        emailInput.textContent = email;
+        newRow.appendChild(emailInput);
+
+        const adminInput = document.createElement("td");
+        adminInput.textContent = admin;
+        newRow.appendChild(adminInput);
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(f) {
+                const imageUrl = f.target.result;
+                const imageInput = document.createElement('td');
+                const newImage = document.createElement("img");
+                newImage.src = imageUrl;
+                newImage.width = 64;
+                newImage.height = 64;
+                imageInput.appendChild(newImage);
+                newRow.appendChild(imageInput);
+                table.appendChild(newRow);
+                
+            }
+            reader.readAsDataURL(file);
+        } else {
+            const imageInput = document.createElement('td');
+            imageInput.textContent = '';
+            newRow.appendChild(imageInput);
+            table.appendChild(newRow);
+        }
+    }
 }
 
 function emptyTable() {
